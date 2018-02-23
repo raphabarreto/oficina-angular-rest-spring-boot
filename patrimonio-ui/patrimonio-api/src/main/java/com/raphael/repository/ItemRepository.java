73f3deb7c2bd029d727c7111f33bda24dc4abbe6
Raphael Barreto <tnt.raphael@gmail.com>
2018-02-22 23:59:57 -0300

    Atualizado os diretórios

--- patrimonio-ui/patrimonio-api/src/main/java/com/raphael/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.raphael.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.raphael.model.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Long>{
-
-}
